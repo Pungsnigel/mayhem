b71401967289c9ffca86fb57afe275e307dda1ae
Jesper Persson <jespert.persson@gmail.com>
2014-01-01 17:04:33 +0100

    Add class for NPC

---
+++ src/com/piggy/mayhem/entity/mob/TestMob.java
@@ -0,0 +1,31 @@
+package com.piggy.mayhem.entity.mob;
+
+import com.piggy.mayhem.entity.Entity;
+import com.piggy.mayhem.graphics.Screen;
+
+public class TestMob extends Mob {
+
+
+	public TestMob(int x, int y, int width, int height) {
+		super(x, y, width, height);
+	}
+
+	@Override
+	public void render(Screen screen) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void didCollide(Entity e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
