9f74421d953ccc4257b60ea70234e6dc70f7f11a
Jesper Persson <jespert.persson@gmail.com>
2014-01-03 03:55:41 +0100

    Update spritesheet for enemy, add random ai

--- src/com/piggy/mayhem/entity/mob/TestMob.java
+++ src/com/piggy/mayhem/entity/mob/TestMob.java
@@ -10,11 +10,12 @@ public class TestMob extends Mob {
 	
 	private int dx = 1;
 	private int dy = 1;
+	private int time = 0; 				// To make random AI look a bit better, use a counter to make sure it doesn't update to often
 
 	private AnimatedSprite animator;
 	private AnimatedSprite down = new AnimatedSprite(16, 16, SpriteSheet.enemy_down, 4);
 	private AnimatedSprite up = new AnimatedSprite(16, 16, SpriteSheet.enemy_up, 4);
-	private AnimatedSprite left= new AnimatedSprite(16, 16, SpriteSheet.enemy_left, 4);
+	private AnimatedSprite left = new AnimatedSprite(16, 16, SpriteSheet.enemy_left, 4);
 	private AnimatedSprite right = new AnimatedSprite(16, 16, SpriteSheet.enemy_right, 4);
 
 	public TestMob(int x, int y, int width, int height) {
@@ -32,16 +33,16 @@ public class TestMob extends Mob {
 		screen.renderEntity(x, y, this.sprite, false, false);
 	}
 
-	@Override
-	public void didCollide(Entity other){
-		if (other instanceof Projectile) {
-			dx *= -1;
-			dy *= -1;
-		}
-	}
 
 	@Override
 	public void update() {
+		time++;
+		if (time % (random.nextInt(50) + 30) == 0)	{
+			dx = random.nextInt(3) - 1; // Random value between -1 - 1, left, still or right.
+			dy = random.nextInt(3) - 1;
+			if (random.nextInt(3) == 0)
+				dx = dy = 0;
+		}
 		move(dx, dy);
 		
 		if (dir == Direction.DOWN)  animator = down;
@@ -54,4 +55,11 @@ public class TestMob extends Mob {
 		
 	}
 
+	@Override
+	public void didCollide(Entity other){
+		if (other instanceof Projectile) {
+			dx *= -1;
+			dy *= -1;
+		}
+	}
 }
